9a7742c0b212f3ad3d099f2e3c2c9c31b973088f
Raunak Kedia <raunakkedia1@gmail.com>
2023-02-04 14:53:20 +0530

    Basic Form structure & functionality set

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import styles from "@/global.module.css";
+import useMultiStepForm from "./hooks/useMultistepForm";
 
 const App = () => {
+	const {
+		steps,
+		currentStepIndex,
+		step,
+		isFirstStep,
+		isLastStep,
+		back,
+		next,
+	} = useMultiStepForm([<div>One</div>, <div>Two</div>, <div>Three</div>]);
+
 	return (
 		<div className={styles.card}>
 			<form>
-				<div></div>
+				<div className={styles.step}>
+					{currentStepIndex + 1} / {steps.length}
+				</div>
+				{step}
+				<div className={styles.actions}>
+					{!isFirstStep && (
+						<button type="button" onClick={back}>
+							Back
+						</button>
+					)}
+					<button type="button" onClick={next}>
+						{isLastStep ? `Finish` : `Next`}
+					</button>
+				</div>
 			</form>
 		</div>
 	);
---
+++ src/hooks/useMultistepForm.ts
@@ -0,0 +1,34 @@
+import { ReactElement, useState } from "react";
+
+const useMultiStepForm = (steps: ReactElement[]) => {
+	const [currentStepIndex, setCurrentStepIndex] = useState<number>(0);
+
+	const next = () => {
+		setCurrentStepIndex(prev => {
+			if (prev >= steps.length - 1) return prev;
+			return prev + 1;
+		});
+	};
+
+	const back = () => {
+		setCurrentStepIndex(prev => {
+			if (prev <= 0) return prev;
+			return prev - 1;
+		});
+	};
+
+	const goTo = (index: number) => setCurrentStepIndex(index);
+
+	return {
+		currentStepIndex,
+		step: steps[currentStepIndex],
+		next,
+		back,
+		goTo,
+		steps,
+		isFirstStep: currentStepIndex === 0,
+		isLastStep: currentStepIndex === steps.length - 1,
+	};
+};
+
+export default useMultiStepForm;
